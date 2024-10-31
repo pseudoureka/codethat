242ffd27828d871b6db67ecee7d9e9284991c4c8
pseudoureka <messiah7179@gmail.com>
2024-10-31 23:05:27 +0900

    npx create-react-app ., npm install react-router-dom@6, npm install classnames, routes, route 사용해서 페이지 링크 설정

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishlistPage from "./pages/WishlistPage";
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="courses" element={<CourseListPage />} />
+          <Route path="courses/react-frontend-development" element={<CoursePage />} />
+          <Route path="wishlist" element={<WishlistPage />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
