0153f17e0aec8322932085ea50c724877017dc92
pseudoureka <messiah7179@gmail.com>
2024-11-01 16:14:55 +0900

    route 하위페이지 나누기, Outlet= children 사용하기, Routes 컴포넌트 안에서는 반드시 Route만 사용해야 함.

--- src/Main.js
+++ src/Main.js
@@ -10,16 +10,20 @@ import QuestionsPage from "./pages/QuestionPage";
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="courses" element={<CourseListPage />} />
-          <Route path="courses/react-frontend-development" element={<CoursePage />} />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path="react-frontend-development" element={<CoursePage />} />
+          </Route>
           <Route path="wishlist" element={<WishlistPage />} />
-          <Route path="questions" element={<QuestionListPage />} />
-          <Route path="questions/616825" element={<QuestionsPage />} />
-        </Routes>
-      </App>
+          <Route path="questions">
+            <Route index element={<QuestionListPage />} />
+            <Route path="616825" element={<QuestionsPage />} />
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
