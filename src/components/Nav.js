88ed0a07fb64eb291cfadb71cd019ccae58855ec
pseudoureka <messiah7179@gmail.com>
2024-11-05 13:46:25 +0900

    Link 컴포넌트 적용중(카탈로그, 커뮤니티, 로고)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,33 @@
-import Container from './Container';
-import UserMenu from './UserMenu';
-import logoImg from '../assets/logo.svg';
-import styles from './Nav.module.css';
+import Container from "./Container";
+import UserMenu from "./UserMenu";
+import logoImg from "../assets/logo.svg";
+import styles from "./Nav.module.css";
+import { Link, NavLink } from "react-router-dom";
+
+function getStyles({ isActive }) {
+  if (isActive) {
+    return { textDecoration: "underline" };
+  }
+}
 
 function Nav() {
   return (
     <div className={styles.nav}>
       <Container className={styles.container}>
-        <img src={logoImg} alt="Codethat Logo" />
+        <Link to="/">
+          <img src={logoImg} alt="Codethat Logo" />
+        </Link>
         <ul className={styles.menu}>
-          <li>카탈로그</li>
-          <li>커뮤니티</li>
+          <li>
+            <NavLink to="courses" style={getStyles}>
+              카탈로그
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="questions" style={getStyles}>
+              커뮤니티
+            </NavLink>
+          </li>
           <li>
             <UserMenu />
           </li>
