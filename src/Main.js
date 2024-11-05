2d65467c857419b6432ec4726f04aefac9e4027f
pseudoureka <messiah7179@gmail.com>
2024-11-05 16:10:57 +0900

    Route 하위 항목 만들기

--- src/Main.js
+++ src/Main.js
@@ -4,18 +4,26 @@ import HomePage from "./pages/HomePage";
 import CourseListPage from "./pages/CourseListPage";
 import CoursePage from "./pages/CoursePage";
 import WishlistPage from "./pages/WishlistPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import QuestionPage from "./pages/QuestionPage";
 
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="courses" element={<CourseListPage />} />
-          <Route path="courses/react-frontend-development" element={<CoursePage />} />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path="courses/react-frontend-development" element={<CoursePage />} />
+          </Route>
+          <Route path="questions">
+            <Route index element={<QuestionListPage />} />
+            <Route path="616825" element={<QuestionPage />} />
+          </Route>
           <Route path="wishlist" element={<WishlistPage />} />
-        </Routes>
-      </App>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
